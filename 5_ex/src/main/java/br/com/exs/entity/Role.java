55e9fadff29c6b685877dd4f62e4cd7021193804
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2019-09-10 11:49:24 -0300

    first commit

---
+++ 5_ex/src/main/java/br/com/exs/entity/Role.java
@@ -0,0 +1,34 @@
+package br.com.exs.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
